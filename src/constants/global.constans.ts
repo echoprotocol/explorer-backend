b1b4bb36d4da337c104ad83e6698ce1c4be0b5ef
Alina Lozyuk <32218750+AlinaLoz@users.noreply.github.com>
2019-10-14 17:25:36 +0300

    [EEX-636] up echojs-lib, fix contants, change version ts, up version 1.0.2 (#10)
    * [EEX-636] up echojs-lib, fix contants, change version ts
    
    * [EEX-636] up version

--- src/constants/global.constans.ts
+++ src/constants/global.constans.ts
@@ -2,8 +2,8 @@ import { constants }  from 'echojs-lib';
 
 export const PATH_TO_PUBLIC = 'public';
 export const PATH_TO_ICONS = 'icons';
-export const ACCOUNT_PREFIX = `${constants.CHAIN_TYPES.RESERVED_SPACES.PROTOCOL_IDS}.${constants.OBJECT_TYPES.ACCOUNT}.`;
-export const CONTRACT_PREFIX = `${constants.CHAIN_TYPES.RESERVED_SPACES.PROTOCOL_IDS}.${constants.OBJECT_TYPES.CONTRACT}.`;
+export const ACCOUNT_PREFIX = `${constants.CHAIN_TYPES.RESERVED_SPACE_ID.PROTOCOL}.${constants.PROTOCOL_OBJECT_TYPE_ID.ACCOUNT}.`;
+export const CONTRACT_PREFIX = `${constants.CHAIN_TYPES.RESERVED_SPACE_ID.PROTOCOL}.${constants.PROTOCOL_OBJECT_TYPE_ID.CONTRACT}.`;
 export const TOKEN_ECHOJS = 'ECHOJS_SERVICE';
 export const TOKEN_GRAPHQL = 'GRAPHQL';
 
